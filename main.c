34747d7df6831bf4a85734a28042d8fca9cca8f7
Hammouche Faiz <faiz.hammouche@smile.fr>
2023-04-21 12:10:21 +0200

    Final version of ULP_ADC

--- main.c
+++ main.c
@@ -12,8 +12,12 @@
 
 
 
+
+
 // Our header
 #include "ulp_main.h"  //bibliotheque qui contient les déclarations et défintions des varibles et fonctions initialisées dans le Processeur Ultra Low Power
+
+#define MicroSecondes_to_Secondes 1000000
 
 // Unlike the esp-idf always use these binary blob names
 extern const uint8_t ulp_main_bin_start[] asm("_binary_ulp_main_bin_start");
@@ -23,6 +27,8 @@ static void init_run_ulp(esp_adc_cal_characteristics_t*);   //initialisation des
 static void start_ulp_program();                            // on charge le binaire généré par le code assembleur  
 static void temperature (float* , int);
 RTC_DATA_ATTR esp_adc_cal_characteristics_t adc_chars;    //initialise les charactéristiques de l'adc dans la mémoire RTC
+
+
 
 void app_main() 
 { 
@@ -34,25 +40,23 @@ void app_main()
     init_run_ulp(&adc_chars);
     start_ulp_program();
   }
-  uint32_t res = esp_adc_cal_raw_to_voltage(ulp_adc_value&0xFFFF, &adc_chars); //conversion adc-voltage 
-  temperature(&temp,res); //conversion temperature-voltage
-  if(temp >28)
+ 
+  if((ulp_flag_alarme&0xFFFF)==1)
+  //while(1)
   {
-    vTaskDelay(100/portTICK_PERIOD_MS);
-    ESP_LOGE("[Main]","Température = %f°C  => Alarme! ",temp);
-    while(temp>28)
-    {
-      res = esp_adc_cal_raw_to_voltage(ulp_adc_value&0xFFFF, &adc_chars); 
-      temperature(&temp,res); 
-      vTaskDelay(10/portTICK_PERIOD_MS);
-    }
-    ESP_LOGE("[Main]","Température en dessous du seuil");
+    uint32_t res = esp_adc_cal_raw_to_voltage(ulp_adc_value&0xFFFF, &adc_chars); //conversion adc-voltage 
+    temperature(&temp,res); //conversion temperature-voltage
+    ESP_LOGE("[Main]","Température = %f°C => Alarme!",temp);
+    ESP_LOGE("[Main]","Going to sleep, Alarme already set");
+    esp_err_t err2= ulp_run((&ulp_entry2 - RTC_SLOW_MEM));
+    ESP_ERROR_CHECK(err2);
+    esp_deep_sleep_start();
   }
   esp_err_t err = ulp_run((&ulp_entry - RTC_SLOW_MEM));
   ESP_ERROR_CHECK(err);
   vTaskDelay(100/portTICK_PERIOD_MS);
-  ESP_LOGE("[Main]","Going to sleep");
-  esp_deep_sleep_start();                    //passage en mode deep sleep
+  ESP_LOGE("[Main]","Going to sleep, no issues"); 
+  esp_deep_sleep_start();
 }
 
 
@@ -66,17 +70,17 @@ static void init_run_ulp(esp_adc_cal_characteristics_t* adc_chars)
     esp_adc_cal_characterize(ADC_UNIT_1,ADC_ATTEN_DB_11,ADC_BITWIDTH_12,0,adc_chars);
     adc1_ulp_enable(); 
 }
-
 static void start_ulp_program()
 {
+   ulp_set_wakeup_period(0,1*MicroSecondes_to_Secondes);
+    // REG_SET_FIELD(SENS_ULP_CP_SLEEP_CYC0_REG, SENS_SLEEP_CYCLES_S0,150000*15);
     /* Start the program */
     vTaskDelay(100/portTICK_PERIOD_MS);
     ulp_load_binary(0, ulp_main_bin_start, (ulp_main_bin_end - ulp_main_bin_start) / sizeof(uint32_t));
 }
 
 
-static void temperature (float *temp,int res)                //fonction qui convertit de la tension d'entree en temperature
+static void temperature (float *temp,int res)                //fonction qui de la tension d'entree en temperature
 {
     *temp = (res - 500.0)/10;
 }
-
